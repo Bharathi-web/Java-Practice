1ae3fca8479d2a92983a3e371fafcd38787727f3
Bharathi-web <bharathiyoga002@gmail.com>
2025-07-09 11:42:22 +0530

    Add files via upload

---
+++ CharacterArray.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class CharacterArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch[]= {'a','B','5','c',' ','3'};
+		for (int i=0;i<ch.length;i++) {
+			if(ch[i]!=' ') {
+				System.out.println(ch[i]);
+			}
+		}
+
+	}
+}
